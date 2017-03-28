9658e0a9bf82936e90f099a2b64c5e4969321d4d
Felipe G. Hespanhol <felipe.totalcommit.com>
2017-03-28 19:53:12 -0300

    first commit

---
+++ js/stateMain.js
@@ -0,0 +1,16 @@
+var StateMain= {
+  preload: function() {
+    // load images into library
+  },
+
+  create: function() {
+    // set up objects, variables
+    // sounds, text
+    // good guys, explosions
+    console.log("Ready!");
+  },
+
+  update: function() {
+    // constant running loop
+  }
+}
---
+++ js/stateTitle.js
@@ -0,0 +1,38 @@
+var StateTitle = {
+  preload: function() {
+    // load images into library
+    game.load.image("logo", "images/title/gameLogo.png");
+    game.load.spritesheet('buttons', 'images/ui/buttons.png', 265, 75, 8);
+    if (screen.width < 800) {
+      game.scale.forceOrientation(false, true);
+    }
+  },
+
+  create: function() {
+    // set up objects, variables
+    // sounds, text
+    // good guys, explosions
+    this.logo = game.add.sprite(game.world.centerX, 180, "logo");
+    this.logo.anchor.set(0.5, 0.5);
+    this.setListeners();
+  },
+
+  setListeners: function() {
+    if (screen.width < 800) {
+      game.scale.enterIncorrectOrientation.add(this.wrongWay, this);
+      game.scale.leaveIncorrectOrientation.add(this.rightWay, this);
+    }
+  },
+
+  wrongWay: function() {
+    document.getElementById("wrongWay").style.display = "block";
+  },
+
+  rightWay: function() {
+    document.getElementById("wrongWay").style.display = "none";
+  },
+
+  update: function() {
+    // constant running loop
+  }
+}
